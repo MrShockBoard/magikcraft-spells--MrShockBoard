f553f36b55fc11e3ce9b59a3e813b0c88c470197
MrShockBoard <logan@spinnys.com>
2017-12-01 10:35:30 +0000

    Update hi.ts

---
+++ hi.ts
@@ -0,0 +1,29 @@
+const magik = magikcraft.io;
+
+function hi(t = 5) {
+	const = times t * 1000 / 300;
+    let n = times
+    makik.settimeout(shield, 300);
+    magik.dixit('magik shield!')
+    function shield() {
+        n --;
+        const location =magik.hic();
+        const nearbyEntities = location.getworld().getnearbyEntities(loction, 8, 8, 8)
+        nearbyEntities.forEach(entity => toss(entitys));
+        if (n>0) {
+            magik.setTimeout(shield, 300)
+            } else {
+                magik.dixet('shelied exhausted!');
+                }
+        }
+}
+
+Function toss(entity){
+    function toss(entity){
+    if (entity.getName() == magik.getSneder().getName()) {
+        return;
+    }
+    var vector = java.type('org.bukkit.util.vector');
+    entity.setVeloctity(new vector(2,2,2));
+    }
+}
