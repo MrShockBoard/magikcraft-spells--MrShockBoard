78c3eb44658bfdad9bfc20a2185343c5efa323fd
MrShockBoard <logan@spinnys.com>
2017-12-17 04:23:38 +0000

    Update GM.ts

---
+++ GM.ts
@@ -0,0 +1,11 @@
+function GM(mode="c") {
+const me = magik.getSender();
+const GameMode = Java.type("org.bukkit.GameMode");
+const modes = {
+   c: GameMode.CREATIVE,
+   s: GameMode.SURVIVAL,
+   x: GameMode.SPECTATOR
+}
+me.setGameMode(modes[mode])
+
+}
